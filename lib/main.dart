0a3a1b9b793613af5923ed8ff9280f386f74f30d
roseliaevanny <roseliasucipto8@gmail.com>
2025-11-19 15:31:38 +0700

    tutorial 9 workflow

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,9 @@ class MyApp extends StatelessWidget {
           //
           // This works for code too, not just values: Most code changes can be
           // tested with just a hot reload.
-            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-          .copyWith(secondary: Colors.blueAccent[400]),
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.blue,
+          ).copyWith(secondary: Colors.blueAccent[400]),
         ),
         home: const LoginPage(),
       ),
